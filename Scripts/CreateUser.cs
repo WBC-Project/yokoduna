e25c6546088e6c628778325cddbb902828ec159f
kazuya-kuramoto <kazuya.kuramoto@o-10459-mac.local>
2018-07-15 20:14:48 +0900

    create userのオブザーブを定義

--- Scripts/CreateUser.cs
+++ Scripts/CreateUser.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 
 namespace Yokoduna {
     // "CreateUser" of rest client's invoke system
+    // false => error , true => success
     public class _createUser {
-        public _createUser(User user, Config conf, bool throwHandle = false) {
+        public _createUser(User user, Config conf, Subject<bool> unit, bool throwHandle = false) {
+            if (_checkCreatedUser()) {
+                Debug.LogError("[Yokoduna Error] Creating User Registed! Can not duplication user infomations!");
+                return;
+            }
             string uri = String.Format("{0}api/newuser?_api_token={1}&product_id={2}&name={3}&mail={4}&pass={5}",
                 conf.baseURL,
                 conf.apikey,
@@ -23,8 +28,10 @@ namespace Yokoduna {
                 if ( info.isError ) {
                     if (!throwHandle) Debug.LogError(String.Format("[Yokoduna Error] Create User: {0}",info.error));
                     else Debug.LogWarning(String.Format("[Yokoduna Error] Create User: {0}",info.error));
+                    unit.OnNext(false);
                     return;
                 }
+                unit.OnNext(true);
             });
         }
 
@@ -32,9 +39,9 @@ namespace Yokoduna {
         private bool _checkCreatedUser () {
             string userID = PlayerPrefs.GetString("YokodunaPlayerRegistedID");
             if (userID != "") {
-                return false;
+                return true;
             }
-            return true;
+            return false;
         }
     }
 }
